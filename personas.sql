c77f8cb0ae8cb1ab22c834acff3730df1f77b662
joregesosa <joregesosa@gmail.com>
2023-11-24 16:19:27 -0400

    insert personas

---
+++ personas.sql
@@ -0,0 +1,16 @@
+INSERT INTO datos_personales (nombres, apellidos, identificacion, telefono, pais, edad, hijos, estado) VALUES
+('Juan', 'Pérez', '123456789', '1234567890', 'México', 30, 1, 1),
+('María', 'González', '987654321', '0987654321', 'España', 25, 0, 1),
+('Carlos', 'Martínez', '456789123', '4567891230', 'Argentina', 40, 0, 0),
+('Ana', 'López', '789123456', '7891234560', 'Colombia', 35, 1, 0),
+('Luis', 'Ramírez', '555111333', '5551113330', 'Perú', 28, 1, 1),
+('Laura', 'Sánchez', '222333444', '2223334440', 'Chile', 33, 0, 1),
+('Roberto', 'Fernández', '777888999', '7778889990', 'Uruguay', 45, 1, 0),
+('Sofía', 'Díaz', '666444222', '6664442220', 'Costa Rica', 29, 0, 1),
+('Diego', 'Hernández', '333555777', '3335557770', 'Ecuador', 31, 1, 0),
+('Valeria', 'Gómez', '999000111', '9990001110', 'Bolivia', 27, 0, 0),
+('Gabriel', 'Torres', '444222666', '4442226660', 'Paraguay', 36, 1, 1),
+('Natalia', 'Martín', '111999888', '1119998880', 'Panamá', 32, 0, 1),
+('Elena', 'Ruiz', '888777666', '8887776660', 'Venezuela', 34, 1, 0),
+('Alejandro', 'Silva', '555777888', '5557778880', 'El Salvador', 26, 0, 0),
+('Lucía', 'Luna', '777444555', '7774445550', 'Guatemala', 30, 1, 1);
